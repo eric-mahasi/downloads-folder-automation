dd3ca66e2d7e0a373cf6fa58ed0096f2a416e6af
Eric Mahasi <mahasieric@gmail.com>
2021-09-22 16:07:43 +0300

    Added functionality to loop through a folder. Yet to resolve permission error.

--- classifier.py
+++ classifier.py
@@ -1,7 +1,16 @@
 import pathlib
+import shutil
 from pathlib import Path
 
 downloads_folder_path = Path("/Users/ericm/Downloads")
-contents = pathlib.Path(downloads_folder_path).iterdir()
-for path in contents:
-    print(path)
+programs_path = Path("/Users/ericm/Downloads/Programs")
+file_type_variation_list = ['.exe', '.ini', '.zip', '.msi', '.iso', '.torrent']
+for file in downloads_folder_path.iterdir():
+    if file.is_file():
+        extension = file.suffix
+        if extension not in file_type_variation_list:
+            file_type_variation_list.append(extension)
+        if extension == '.exe':
+            shutil.move(downloads_folder_path, programs_path)
+
+print(file_type_variation_list)
