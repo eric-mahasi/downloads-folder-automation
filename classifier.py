432566e39d47acd75f4c05ac61b0152eb8e5d52f
Eric Mahasi <mahasieric@gmail.com>
2021-09-23 13:10:32 +0300

    Renamed variable for uniformity.

--- classifier.py
+++ classifier.py
@@ -2,10 +2,10 @@ import pathlib
 import shutil
 from pathlib import Path
 
-downloads_folder_path = Path("/Users/ericm/Downloads")
+downloads_path = Path("/Users/ericm/Downloads")
 programs_path = Path("/Users/ericm/Downloads/Programs")
 file_type_variation_list = ['.exe', '.ini', '.zip', '.msi', '.iso', '.torrent']
-for file in downloads_folder_path.iterdir():
+for file in downloads_path.iterdir():
     if file.is_file():
         extension = file.suffix
         if extension not in file_type_variation_list:
