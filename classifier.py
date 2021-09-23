a45067aec298823e39d2c2c1c831895728d42fa7
Eric Mahasi <mahasieric@gmail.com>
2021-09-23 14:07:49 +0300

    Refactored functionality for moving a file into its own method.

--- classifier.py
+++ classifier.py
@@ -18,15 +18,21 @@ doc_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx')
 music_types = ('.mp3', '.wav')
 video_types = ('.mp4', '.gif',)
 picture_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff')
+
+
+def move_file(dest_path):
+    if os.path.isdir(dest_path):
+        shutil.move(file, dest_path)
+    else:
+        os.mkdir(dest_path)
+        shutil.move(file, dest_path)
+        
+        
 for file in downloads_path.iterdir():
     if file.is_file():
         extension = file.suffix
         if extension in program_types:
-            if os.path.isdir(programs_path):
-                shutil.move(file, programs_path)
-            else:
-                os.mkdir(programs_path)
-                shutil.move(file, programs_path)
+            move_file(programs_path)
         elif extension in compressed_types:
             shutil.move(file, compressed_path)
         elif extension in doc_types:
