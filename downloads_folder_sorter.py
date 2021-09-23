122f843df108b8dfd32f2d264956d7d7fc70c9e8
Eric Mahasi <mahasieric@gmail.com>
2021-09-23 14:45:34 +0300

    Added docstrings.

--- downloads_folder_sorter.py
+++ downloads_folder_sorter.py
@@ -1,3 +1,12 @@
+"""Downloads folder sorter
+
+This script iterates through the files in my downloads folder and places each file in its appropriate folder.
+This file contains the following functions:
+
+    * move_file - checks if the destination folder exists, creates it if it doesn't, then moves a file into it
+    * sort folder - iterates through the files in the folder
+"""
+
 import os.path
 import shutil
 from pathlib import Path
@@ -20,6 +29,14 @@ picture_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff')
 
 
 def move_file(file, dest_path):
+    """Checks if the destination folder exists, creates it if it doesn't, then moves a file into it
+    Parameters
+    ----------
+    file : Path
+        the path to a file
+    dest_path : Path
+        the path to the destination folder
+    """
     if os.path.isdir(dest_path):
         shutil.move(file, dest_path)
     else:
@@ -28,6 +45,7 @@ def move_file(file, dest_path):
 
 
 def sort_folder():
+    """Iterates through the files in the folder"""
     for file in downloads_path.iterdir():
         if file.is_file():
             extension = file.suffix
