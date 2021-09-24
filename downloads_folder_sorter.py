476efc70fea29bacdc9109873efeb7fa6ae8849e
Dara Dadachanji <dara.dadachanji@gmail.com>
2021-09-23 19:30:21 -0700

    refactored to use a JSON config file and be somewhat folder agnostic

--- downloads_folder_sorter.py
+++ downloads_folder_sorter.py
@@ -8,31 +8,41 @@ This file contains the following functions:
     * move_file - checks if the destination folder exists, creates it if it doesn't, then moves a file into it
     * sort_folder - iterates through the files in the folder
 """
-
+import os
 import os.path
 import shutil
-from pathlib import Path
+import pathlib
+import json
 
-user = os.getenv('USERNAME')
-downloads_path = Path("/Users/{}/Downloads".format(user))
-programs_path = Path("/Users/{}/Downloads/Programs".format(user))
-compressed_path = Path("/Users/{}/Downloads/Compressed".format(user))
-documents_path = Path("/Users/{}/Downloads/Documents".format(user))
-music_path = Path("/Users/{}/Downloads/Music".format(user))
-video_path = Path("/Users/{}/Downloads/Video".format(user))
-pictures_path = Path("/Users/{}/Downloads/Pictures".format(user))
-other_path = Path("/Users/{}/Downloads/Other".format(user))
-
-program_types = ('.exe', '.pkg', '.dmg', '.msi')
-compressed_types = ('.zip', '.rar')
-doc_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx')
-music_types = ('.mp3', '.wav')
-video_types = ('.mp4', '.mkv',)
-picture_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff')
+with open('categories.json', encoding='UTF-8') as f:
+    CATEGORIES = json.load(f)
 
 
-def move_file(file, dest_path):
-    """Checks if the destination folder exists, creates it if it doesn't, then moves a file into it
+def main():
+    username = os.getenv('USERNAME')
+    downloads_folder = pathlib.Path(f'/Users/{username}/Downloads')
+    sort_folder(downloads_folder)
+
+
+def sort_folder(folder: pathlib.Path):
+    """Iterates through the files in the folder"""
+    for file in folder.iterdir():
+        sort_file(file)
+
+
+def sort_file(file: pathlib.Path):
+    """moves an individual file into the correct folder"""
+    if file.is_file():
+        for category in CATEGORIES:
+            if file.suffix in category['extensions']:
+                destination = file.parent.joinpath(category['name'])
+                move_file(source=file, destination=destination)
+
+
+def move_file(source: str, destination: str):
+    """Checks if the destination folder exists, 
+    creates it if it doesn't, 
+    then moves a file into it
     Parameters
     ----------
     file : Path
@@ -40,33 +50,12 @@ def move_file(file, dest_path):
     dest_path : Path
         the path to the destination folder
     """
-    if os.path.isdir(dest_path):
-        shutil.move(file, dest_path)
+    if os.path.isdir(destination):
+        shutil.move(source, destination)
     else:
-        os.mkdir(dest_path)
-        shutil.move(file, dest_path)
-
-
-def sort_folder():
-    """Iterates through the files in the folder"""
-    for file in downloads_path.iterdir():
-        if file.is_file():
-            extension = file.suffix
-            if extension in program_types:
-                move_file(file, programs_path)
-            elif extension in compressed_types:
-                move_file(file, compressed_path)
-            elif extension in doc_types:
-                move_file(file, documents_path)
-            elif extension in music_types:
-                move_file(file, music_path)
-            elif extension in video_types:
-                move_file(file, video_path)
-            elif extension in picture_types:
-                move_file(file, pictures_path)
-            else:
-                move_file(file, other_path)
+        os.mkdir(destination)
+        shutil.move(source, destination)
 
 
 if __name__ == '__main__':
-    sort_folder()
+    main()
