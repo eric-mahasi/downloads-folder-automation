60721876c967dd7c651d8f57cea6e7d758be3420
Vinícius Amorim <vamorim.dev@gmail.com>
2022-08-11 14:32:50 -0300

    adding sorting by extension and ui

--- folder_sorter.py
+++ folder_sorter.py
@@ -6,10 +6,11 @@ This file contains the following functions:
     * sort_folder - iterates through the files in the folder
 """
 
-import shutil
+import os
 import json
+import shutil
 from pathlib import Path
-from tkinter import Tk, filedialog, messagebox
+from tkinter import Tk, Button, filedialog, messagebox, Grid
 
 
 def move_file(file, destination):
@@ -30,13 +31,9 @@ def move_file(file, destination):
         print(e)
 
 
-def sort_folder(folder_path):
-    """Iterates through the files in the folder, sorting them into sub-folders by extension.
-    Parameters
-    ----------
-    folder_path : Path
-        the path to the folder to be organized
-    """
+def sort_folder_by_categories():
+    """Iterates through the files in the folder, sorting them into sub-folders by extension."""
+    folder_path = select_folder()
     with open("config.json", encoding="utf-8") as f:
         categories = json.load(f)
 
@@ -53,15 +50,50 @@ def sort_folder(folder_path):
     messagebox.showinfo("File Sort", "All files were sorted")
 
 
+def sort_folder_by_extensions():
+    folder_path = select_folder()
+    list_ = os.listdir(folder_path)
+    for file_ in list_:
+        _, ext = os.path.splitext(file_)
+        ext = ext[1:]
+        if ext == "":
+            continue
+        if not os.path.exists(f"{folder_path}/{ext}"):
+            os.makedirs(f"{folder_path}/{ext}")
+        shutil.move(f"{folder_path}/{file_}", f"{folder_path}/{ext}/{file_}")
+    messagebox.showinfo("File Sort", "All files were sorted")
+
+
 def select_folder():
-    """
-    Opens a file dialog to select a folder.
-    """
+    """Opens a file dialog to select a folder."""
     root = Tk()
     root.withdraw()
     root.attributes("-topmost", True)
     return Path(filedialog.askdirectory())
 
 
+def user_interface():
+    root = Tk()
+    root.title("Folder Sorter")
+    root.geometry("250x150")
+
+    x = (root.winfo_screenwidth() / 2) - (root.winfo_reqwidth() / 2)
+    y = (root.winfo_screenheight() / 2) - (root.winfo_reqheight() / 2)
+    root.geometry("+%d+%d" % (x, y))
+
+    Grid.rowconfigure(root, 0, weight=1)
+    Grid.columnconfigure(root, 0, weight=1)
+
+    b1 = Button(root, text="Sort by Categories", command=sort_folder_by_categories)
+    b2 = Button(root, text="Sort by Extensions", command=sort_folder_by_extensions)
+    bl = [b1, b2]
+    for row_no, _ in enumerate(bl):
+        Grid.rowconfigure(root, row_no, weight=1)
+    b1.grid(row=0, column=0, sticky="nsew")
+    b2.grid(row=1, column=0, stick="nsew")
+
+    root.mainloop()
+
+
 if __name__ == "__main__":
-    sort_folder(select_folder())
+    user_interface()
